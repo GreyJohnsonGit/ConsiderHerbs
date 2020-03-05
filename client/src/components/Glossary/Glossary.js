bda55aae6e6cf4e0622108809c177c17f91e85ca
olgacabrera <olga.cabrerahern@ufl.edu>
2020-03-05 02:32:35 -0500

    Attempting to Fix Image covering the letters

--- client/src/components/Glossary/Glossary.js
+++ client/src/components/Glossary/Glossary.js
@@ -2,39 +2,42 @@ import React from 'react';
 import './Glossary.css'
 import IntroBar from './intro.png'
 import Terms from './terms.json'
-//import * as fs from 'fs'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-/*let terms;
-fs.readFile('terms.json', 'utf8',(data, err)=>{
-    terms=JSON.parse(data);
-});
-*/
 const Glossary =()=>{
     return(
         <div>
-            <img className = "intro-image" src={ IntroBar } alt="intro" />
-            <h1>THIS IS THE GLOSSARY PAGE</h1>
-            { Terms.map((glossaryEntry)=>{
-                return(
-                    <div>
-                        <p>{glossaryEntry.title}</p>
-                        <div>
-                            <p> Definition: {glossaryEntry.definition}</p>
+            <div >
+                <img className = "intro-image" src={ IntroBar } alt="intro" />
+            </div>
+            <div >
+                <h1>THIS IS THE GLOSSARY PAGE</h1>
+                { Terms.map((glossaryEntry)=>{
+                    return(
+                        <div className="column1" >
+                            <p>{glossaryEntry.title}</p>
+                            <div>
+                                <p> Definition: {glossaryEntry.definition}</p>
+                            </div>
+                            <div>
+                                <p> Usage: {glossaryEntry.usage}</p>
+                            </div>
+                            
                         </div>
-                        <div>
-                            <p> Usage: {glossaryEntry.usage}</p>
-                        </div>
-                        
-                    </div>
-                )
-            })}
-                
+                    
+                    )
+                })}
+            </div>
         </div>
+        
         ////search bar 
         ////terms components, big letters, plus each term starting wiht that letter, plus definition
         ///letters nav bar to the side 
 
     );
+
+   
 }
 
 export default Glossary;
