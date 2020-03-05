39ad27ecbc942f5935a9adf81aab016e5597410c
olgacabrera <olga.cabrerahern@ufl.edu>
2020-03-04 22:46:23 -0500

    Glossary terms

--- client/src/components/Glossary/Glossary.js
+++ client/src/components/Glossary/Glossary.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import './Glossary.css'
 import IntroBar from './intro.png'
+import Terms from './terms.json'
+//import * as fs from 'fs'
 
-
+/*let terms;
+fs.readFile('terms.json', 'utf8',(data, err)=>{
+    terms=JSON.parse(data);
+});
+*/
 const Glossary =()=>{
     return(
         <div >
             <img className = "intro-image" src={ IntroBar } alt="intro" />
             <h1>THIS IS THE GLOSSARY PAGE</h1>
+            { Terms.map((glossaryEntry)=>{
+                return(
+                    <div>
+                        <p>{glossaryEntry.title}</p>
+                        <div>
+                            <p> Definition: {glossaryEntry.definition}</p>
+                        </div>
+                        <div>
+                            <p> Usage: {glossaryEntry.usage}</p>
+                        </div>
+                        
+                    </div>
+                )
+            })}
+                
         </div>
-        ////image here 
         ////search bar 
         ////terms components, big letters, plus each term starting wiht that letter, plus definition
         ///letters nav bar to the side 
