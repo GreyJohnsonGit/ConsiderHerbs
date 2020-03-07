51ad50e67bb9c351f09c64a2036d9de73b089bd7
Keanu Budham <kbudham@ufl.edu>
2020-03-06 19:17:12 -0500

    Update glossaryentry.js

--- server/models/glossaryentry.js
+++ server/models/glossaryentry.js
@@ -1,8 +1,10 @@
-import mongoose from 'mongoose';
-const GlossarySchema = new mongoose.Schema({
-  title : {type:String, required: true},
-  definition: String, 
-  usage: String
-});
+mongoose = require('mongoose');
 
-export default mongoose.model('GlossaryEntry', GlossarySchema);
+const schema = new mongoose.Schema({
+  title : {type: String, required: true}, 
+  definition : {type: String, required: true},
+  usage : {type: String}
+} 
+  );
+
+module.exports = mongoose.model('Glossaries', schema);
