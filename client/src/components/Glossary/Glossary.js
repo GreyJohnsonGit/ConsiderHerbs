fed8c540b251afa00db7f9938651aceb585dc595
cosimawest <44038882+cosimawest@users.noreply.github.com>
2020-03-07 11:39:08 -0500

    fontssss bbyyyy

--- client/src/components/Glossary/Glossary.js
+++ client/src/components/Glossary/Glossary.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Glossary.css';
 import IntroBar from './intro.png';
+import Weebs from './Rosemarys.JPG';
 import TermInfo from "./glossary_components/TermInfo";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const Glossary =()=>{
     return(
-        <div>
-            <img className = "intro-image" src={ IntroBar } alt="intro" />
+        <div>            
+            <div className = "container">
+                <img src = { Weebs } width = "100%"/>
+                <div class = "text-block">
+                    <div> LET'S TALK ABOUT HERBS! </div>
+                </div>
+            </div>
 
             <div className = "form">
                 <form>
