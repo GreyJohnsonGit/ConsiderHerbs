7bdc502501584ec4e11b6504e1e97ceee2be719d
Nathan Andreo <nathan.andreo33@gmail.com>
2020-03-09 13:10:44 -0400

    footer with temp icons and styling

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './Footer.css';
+
 import TempLogo from "./temp_logo.png";
-//import { FaMobileAlt, FiPhoneCall, AiOutlineMail, IoIosGlobe, FiMapPin } from 'react-icons/fa';
+
+import { FaMobileAlt } from 'react-icons/fa';
+import { FiPhoneCall, FiMapPin } from 'react-icons/fi';
+import { AiOutlineMail } from 'react-icons/ai';
+import { IoIosGlobe } from 'react-icons/io';
+import { IconContext } from 'react-icons/lib/cjs';
+
+const iconSize = '1.75em'
 
 const Footer=()=>{
     return(
@@ -12,8 +20,23 @@ const Footer=()=>{
             {/* <img className = "footer-logo" src={ TempLogo } alt="logo" /> */}
             <div className="footer-container">
                 <div className="text-container">Consider Herbs</div>
-                <div className="link-container">Links</div>
+                <div className="link-container">
+                    <h1>Fast Links:</h1>
+                    <a href='/Home'>Home</a>
+                    <a href='/Forum'>Forum</a>
+                    <a href='/DidYouKnow'>Did you know?</a>
+                    <a href='/Glossary'>Glossary</a>
+                    <a href='/About'>About</a>
+                    <a href='/Schedule'>Schedule</a>
+                </div>
                 <div className="icon-container">
+                    <IconContext.Provider value={{ className: 'react-icons' }}>
+                            <FaMobileAlt size={iconSize} className='react-icons'/>
+                            <FiPhoneCall size={iconSize} className='react-icons'/>
+                            <AiOutlineMail size={iconSize} className='react-icons'/>
+                            <IoIosGlobe size={iconSize} className='react-icons'/>
+                            <FiMapPin size={iconSize} className='react-icons'/>
+                    </IconContext.Provider>
                 </div>
                 <div className="disclaim-container">
                     <p>
