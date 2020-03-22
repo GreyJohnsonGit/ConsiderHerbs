7a330bff1531d88f10eca922d149515e4bd6a1f1
Nathan Andreo <nathan.andreo33@gmail.com>
2020-03-22 12:46:15 -0400

    new thread layout

--- client/src/components/Forum/Forum.js
+++ client/src/components/Forum/Forum.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ForumPreview from './forum_components/ForumPreview.js';
 import Thread from './forum_components/Thread.js';
 import DidYouKnow from './forum_components/DidYouKnow.js';
+import NewThread from './forum_components/NewThread.js';
 import './Forum.css';
 
 const Forum =()=>{
+    const [ showPopup, setShowPopup ] = useState(0);
+
+    const toggleShowPopup = () => {
+        setShowPopup(!showPopup);
+    };
+
     return(
         <div>        
             <div className="image-container">
                 <img src='http://picsum.photos/1440/91' style={{width:'100%',height:'auto'}}/>
                 <div className='forum-title'><span>Connect With The Community!</span></div>
             </div>
+            { showPopup ? <NewThread closeFn={toggleShowPopup}/> : null}
             <div className="forum-content">
                 <div className="forum-column-1">
+                    <div className="search" id="search_bar">
+                        <form>
+                            <input type="text" placeholder="Search Threads..."/>
+                            <button type="submit">Search</button>
+                            <button type='button' onClick={toggleShowPopup}>New Thread</button>
+                        </form>
+                    </div>
                     <Switch>
                         <Route exact path='/Forum' component={ForumPreview} />
                         <Route path={`/Forum/:threadId`} component={Thread} />
