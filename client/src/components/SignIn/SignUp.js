f697c3424e6b81aa6c23be0c60351191864d3cd6
GreyJohnsonGit <contact.grey.johnson@gmail.com>
2020-03-26 11:22:51 -0400

    Added Signin/up. Added CORS management.

--- client/src/components/SignIn/SignUp.js
+++ client/src/components/SignIn/SignUp.js
@@ -1,12 +1,50 @@
 import React, {useState} from 'react'
 import { MdClose } from 'react-icons/md';
+import Axios from 'axios';
+
 import './SignIn.css'
 
 const SignUp = () =>{
-    const [signedUp, setSignedUp]= useState(0) 
-    
-   const SigndUpUser =()=>{
-        setSignedUp(!signedUp)
+    const [signedUp, setSignedUp] = useState(false);
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const usernameHandleChange = event => {
+        setUsername(event.target.value);
+    }
+    const emailHandleChange = event => {
+        setEmail(event.target.value);
+    }
+    const passwordHandleChange = event => {
+        setPassword(event.target.value);
+    }
+    const confirmPasswordHandleChange = event => {
+        setConfirmPassword(event.target.value);
+    }
+    const signedUpHandleChange = event => {
+        setSignedUp(!signedUp);
+    }
+
+    const attemptLogin = event => {
+        Axios.post(
+            'http://consider-herbs.herokuapp.com/api/Authentication/SignUp', //DEBUG ADDRESS
+            {
+                username: username,
+                email: email,
+                password: password,
+                method: 'Email'
+            },
+            {}
+        )
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.error(err)
+        });
+        event.preventDefault();
     }
    
     return(
@@ -15,7 +53,7 @@ const SignUp = () =>{
             <div>
                 <div className='message-popup'>
                     <div className='message-inner'>
-                        <MdClose className='new-thread-close-icon' size='1em' onClick={SigndUpUser} />
+                        <MdClose className='new-thread-close-icon' size='1em' onClick={signedUpHandleChange} />
                         <form >
                             <div> You're all set!  </div>
                         </form>
@@ -28,12 +66,12 @@ const SignUp = () =>{
            <div className = "green-bar"> &nbsp; </div>
             <form className = "input-container"> 
                 <font size="7"> Sign Up </font>
-                <input placeholder="Username" className="enter" required/>
-                <input placeholder="Email" className="enter" required/>
-                <input type="password" placeholder="Password" className="enter" required/>
-                <input type="password" placeholder="Confirm Password" className="enter" required/>
+                <input placeholder="Username" className="enter" value={username} onChange={usernameHandleChange} type="text" required/>
+                <input placeholder="Email" className="enter" value={email} onChange={emailHandleChange} type="text" required/>
+                <input placeholder="Password" className="enter" value={password} onChange={passwordHandleChange} type="password" required/>
+                <input placeholder="Confirm Password" className="enter" value={confirmPassword} onChange={confirmPasswordHandleChange} type="password" required/>
 
-                <button type = "submit" className = "sign-in" onClick={SigndUpUser}> Sign Up </button>
+                <button type = "submit" className = "sign-in" onClick={attemptLogin}> Sign Up </button>
 
                 <div className = "or">
                     <hr size = "3"/>
