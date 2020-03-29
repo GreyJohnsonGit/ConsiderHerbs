433e79ef5ce77df33b52b8585eb312173a4a9636
cosimawest <44038882+cosimawest@users.noreply.github.com>
2020-03-28 21:38:17 -0400

    home update

--- client/src/components/Home/home_components/BodyMap.js
+++ client/src/components/Home/home_components/BodyMap.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Male_Front from './Male_Front.png';
 
 const BodyMap = () => {
     return (
-        <div style={{width: '400px'}}>
-            Body
+        <div className="body-column">
+            <span className="body-button-container">
+                <button id="view">FRONT</button>
+                <button id="view">BACK</button>
+                <div id="sex">
+                    <button id="sex-button"><b>♂</b></button>
+                    <button id="sex-button"><b>♀</b></button>
+                    <button id="sex-button">⚲</button>
+                </div>
+            </span>
+            <img alt = "Plants" src = { Male_Front } width = "100%"/>
         </div>
     )
 }
