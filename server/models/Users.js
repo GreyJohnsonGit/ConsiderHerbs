17ce6834cabead475119be1dc2f7aeda089aa070
Keanu Budham <kbudham@ufl.edu>
2020-03-29 14:13:56 -0400

    Update Users.js

--- server/models/Users.js
+++ server/models/Users.js
@@ -3,6 +3,7 @@ bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   username : {type:String, required: true},
+  usertype : {type: String, enum: ['Admin', 'User', 'Subscriber']},
   email : {type: String, required: true, unique: true },
   password : {type : String},
   method : {type: String, enum: ['Email', 'Google', 'Facebook']},
