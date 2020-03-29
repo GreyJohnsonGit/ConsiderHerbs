d96e3e9c2c22b1a088227e4d1cd2e77b8108b461
Keanu Budham <kbudham@ufl.edu>
2020-03-28 20:41:23 -0400

    Update RecipeController.js

--- server/controllers/RecipeController.js
+++ server/controllers/RecipeController.js
@@ -16,12 +16,12 @@ exports.create = function(req,res) {
 
 exports.read = function(req, res) {
     var model = recipeModel;
-    model.find({title: req.params.title}).exec().then(function(docs, err){
+    model.find({name: req.params.name}).exec().then(function(docs, err){
         if(err){
             res.send('error: recipe not found')
         }
         else{
-            //assumes the recipe_id is unique and only one result will be returned - can change to some id param later
+            //assumes the recipe_id is unique and only one result will be returned - can change to some id param 
             res.send(docs[0])
         }
     })
@@ -30,16 +30,16 @@ exports.read = function(req, res) {
 exports.update = function(req, res) {
     var model = recipeModel;
     let item = req.body;
-    model.findOneAndUpdate({title: req.params.title}, item).exec();
+    model.findOneAndUpdate({name: req.params.name}, item).exec();
     //make the response pretty
-    item.title = req.params.title;
+    item.name = req.params.name;
     res.send(item);
 }
 
-//removes recipe entry, expects url param :title to denote which entry to remove
+//removes recipe entry, expects url param :name to denote which entry to remove
 exports.remove = function(req, res) {
     var model = recipeModel;
-    model.find({title: req.params.title}).exec().then(function(docs, err){
+    model.find({name: req.params.name}).exec().then(function(docs, err){
         if(err){
             res.send(err.message);
         }
@@ -47,17 +47,17 @@ exports.remove = function(req, res) {
             res.send('error: Entry not found');
         }
         else{
-            model.find({title: req.params.title}).remove().exec();
+            model.find({name: req.params.name}).remove().exec();
             res.send(docs[0]);
         }
     })
 }
 
-//returns all recipes in the database, sorted alphabetically by title
+//returns all recipes in the database, sorted alphabetically by name
 exports.getAll = function(req, res) {
     var model = recipeModel;
     model.find({}).exec().then(function(docs, err){
-        docs.sort((a,b) => (a.title > b.title) ? 1 : -1);
+        docs.sort((a,b) => (a.name > b.name) ? 1 : -1);
         res.header('Access-Control-Allow-Origin', '*');
         res.send(docs);
     })
@@ -65,8 +65,7 @@ exports.getAll = function(req, res) {
 
 exports.getAllIngredients = function(req,res) {
     var model = recipeModel;
-    model.find({ingredients: req.params.ingredients}).exec().then(function(docs, err){
-        docs.sort((a,b) => (a.title > b.title) ? 1 : -1);
-        res.send(docs);
+    model.find({name: req.params.name}).exec().then(function(docs, err){
+        res.send(docs[0].ingredients);
     })
 }
