3131a5389bcf1b95cb935e50177802d3416d11c5
Keanu Budham <kbudham@ufl.edu>
2020-04-09 12:54:15 -0400

    Create ProductRouter.js

---
+++ server/routes/ProductRouter.js
@@ -0,0 +1,31 @@
+ProductController = require('../controllers/ProductController.js');
+express = require('express'); 
+const ProductRouter = express.Router()
+
+
+
+//returns all entries, vanilla get request
+// GET: /api/Product/
+ProductRouter.get('/', ProductController.getAll)
+
+//calls create, request should send entry in req.body
+// POST: /api/Product/
+ProductRouter.post('/', ProductController.create);
+
+//calls update, request should send updated entry in req.body and have url param :name
+// PUT: /api/Product/:name
+ProductRouter.put('/:name', ProductController.update);
+
+//calls read, request should include url param :name for entry to grab
+// GET: /api/Product/:name
+ProductRouter.get('/:name', ProductController.read);
+
+//calls remove, request should include url param :name for entry to remove
+// DELETE: /api/Product/:name
+ProductRouter.delete('/:name', ProductController.remove);
+
+//returns the price of a Product
+//GET: /api/Product/:name/price
+ProductRouter.get('/:name/price',ProductController.giveProductPrice);
+  
+module.exports = ProductRouter;
