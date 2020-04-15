9398da0ee7278c08cc4af8cd6a6ad2907ad53aab
manthi4 <44146639+manthi4@users.noreply.github.com>
2020-04-15 12:16:32 -0400

    Week view

--- client/src/components/Schedule/Schedule.js
+++ client/src/components/Schedule/Schedule.js
@@ -1,7 +1,7 @@
 import './Schedule.css';
 import React from 'react';
 import Basket from './Basket.png'
-
+import Calandar from './Calandar'
 
 const Schedule =()=>{
     return(
@@ -14,10 +14,9 @@ const Schedule =()=>{
             <div className="sch-below-title"><span>It's just natural!</span></div>
             
             <div className="sch-below-2"><span>Schedule a consultation today!</span></div>
-            
-           
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23d7f09c&amp;ctz=America%2FNew_York&amp;src=b2xnYWRhbmllbGFjQGdtYWlsLmNvbQ&amp;src=dGJzODU2NmxkbDRwdjUxbzUwb2lmN3U5b2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%237986CB&amp;color=%237986CB&amp;color=%2333B679&amp;color=%230B8043" width="800" height="600" frameborder="0" scrolling="no"></iframe>            
-        
+
+            <Calandar/>
+
         </div>
        
     );
