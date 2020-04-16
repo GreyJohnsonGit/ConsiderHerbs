e934199d6c9a94159e3bc83e620fcc6296ce884e
GreyJohnsonGit <contact.grey.johnson@gmail.com>
2020-04-16 16:03:42 -0400

    Fixed Product Controller

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,7 +1,6 @@
 mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    _id : {type: mongoose.Schema.Types.ObjectId},
     name : {type: String, required : true},
     description : {type: String, required : true},
     type  : {type : String, enum: ['ConsiderHerbs', 'Affiliate', 'Suggested']},
