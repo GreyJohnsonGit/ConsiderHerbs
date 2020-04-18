a1c9b0fd83e510e4d598eeca49473a55c85ab9ba
Nathan Andreo <nathan.andreo33@gmail.com>
2020-04-18 16:28:37 -0400

    navbar response

--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './NavBar.css';
 import ConsiderLogo from './ConsiderLogo.png';
 import {useCookies} from 'react-cookie';
 
-// import { IoIosMenu } from 'react-icons/iSo';
+import { IoIosMenu } from 'react-icons/io';
 
 const NavBar = (props) => {
     const [cookies, removeCookie] = useCookies([]);
+    const [navMenu,setNavMenu] = useState(0);
+
+    const toggleNavMenu = () => {
+        setNavMenu(!navMenu);
+    }
 
     const UserButton = () => {
         if(props.user.userLevel) {
@@ -40,7 +45,7 @@ const NavBar = (props) => {
             </Link>
 
             {/* Page Links */}
-            <div className = "nav-items">
+            <div className={navMenu ? "nav-items responsive" : "nav-items"}>
                 <Link className = "nav-link" to='/Home'>HOME</Link>
                 <Link className = "nav-link" to='/Forum'>FORUM</Link> 
                 <Link className = "nav-link" to='/Glossary'>GLOSSARY</Link>
@@ -48,6 +53,10 @@ const NavBar = (props) => {
                 <Link className = "nav-link" to='/Schedule'>SCHEDULE</Link>
                 <Link className = "nav-link-last" to='/Products'>PRODUCTS</Link>
                 <UserButton></UserButton>
+            </div>
+
+            <div className='nav-menu-button-container'>
+                <IoIosMenu className='nav-menu-button' size='2.3em' onClick={toggleNavMenu} />
             </div>
 
         </div>
