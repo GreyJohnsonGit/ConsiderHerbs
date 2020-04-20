00c6c05d485d0e1e1a5fa9cc162a0e66a9846b6e
Nathan Andreo <nathan.andreo33@gmail.com>
2020-04-20 12:25:37 -0400

    forum response

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -203,7 +203,12 @@ const Profile = (props) => {
                                                 })
                                             }
                                             else {
-                                                return "No posts yet!"
+                                                return (
+                                                    <div className='profile-thread-preview-empty'>
+                                                        <div>Oops... You don't have any posts yet!</div>
+                                                        <a href='/Forum'>GO TO FORUM</a>
+                                                    </div>
+                                                )
                                             }
                                         }}
                                     </Async>
