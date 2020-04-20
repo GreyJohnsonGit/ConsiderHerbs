89b060b013300baa8c10c52a66305c814cb7c077
Nathan Andreo <nathan.andreo33@gmail.com>
2020-04-19 23:14:44 -0400

    saved the world

--- server/routes/ProductRouter.js
+++ server/routes/ProductRouter.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-      cb(null, './uploads/');
+      cb(null, '');
     },
     filename: function(req, file, cb) {
       cb(null, file.originalname);
